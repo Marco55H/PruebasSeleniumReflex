b3fed0c398cdfc4a128e307283107001e6d468ab
Marcos <marco.holguin@iesnervion.es>
2025-02-02 14:49:29 +0100

    A subirlo

--- PruebasSelenium/src/reflex/Prueba1.java
+++ PruebasSelenium/src/reflex/Prueba1.java
@@ -19,13 +19,161 @@ public class Prueba1 {
 	}
 
 	@Test
-	void test1() {
+	void existenEnlaceBuscadores() {
 		driver1.get("http://localhost:3000/");
-		WebElement enlace1 = driver1.findElement(By.id("Cursos"));
-		enlace1.click();
+		WebElement enlaceBuscadores = driver1.findElement(By.id("enlaceBuscadores"));
 		
 		String url = driver1.getCurrentUrl();
-		assertEquals("http://localhost:3000/cursos/", url);
+		assertEquals("http://localhost:3000/", url);
+	}
+
+	@Test
+	void existenEnlaceRedes() {
+		driver1.get("http://localhost:3000/");
+		WebElement enlaceRedes = driver1.findElement(By.id("enlaceRedes"));
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/", url);
+	}
+	
+	@Test
+	void existePaginaBuscadores() {
+		driver1.get("http://localhost:3000/buscadores/");
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/buscadores/", url);
+	}
+	
+	@Test
+	void existePaginaRedesSociales() {
+		driver1.get("http://localhost:3000/redes_sociales/");
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/redes_sociales/", url);
+	}
+	
+	@Test
+	void funcionanEnlaceBuscador() {
+		driver1.get("http://localhost:3000/");
+		WebElement enlaceBuscadores = driver1.findElement(By.id("enlaceBuscadores"));
+		enlaceBuscadores.click();
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/", url);
+	}
+	
+	@Test
+	void funcionanEnlaceRedes() {
+		driver1.get("http://localhost:3000/");
+		WebElement enlaceRedes = driver1.findElement(By.id("enlaceRedes"));
+		enlaceRedes.click();
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/", url);
+	}
+	
+	@Test
+	void existenEnlacesDePagBuscador() {
+		driver1.get("http://localhost:3000/buscadores/");
+		WebElement enlaceGoogle = driver1.findElement(By.id("google"));
+		WebElement enlaceBing = driver1.findElement(By.id("bing"));
+		WebElement enlaceBaidu = driver1.findElement(By.id("baidu"));
+		WebElement enlaceVolver = driver1.findElement(By.id("volver"));		
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/buscadores/", url);
+	}
+	
+	@Test
+	void funcionanEnlaceGoogle() {
+		driver1.get("http://localhost:3000/buscadores/");
+		WebElement enlaceGoogle = driver1.findElement(By.id("google"));
+		enlaceGoogle.click();
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("https://www.google.com/", url);
+	}
+	
+	@Test
+	void funcionanEnlaceBing() {
+		driver1.get("http://localhost:3000/buscadores/");
+		WebElement enlaceBing = driver1.findElement(By.id("bing"));
+		enlaceBing.click();
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("https://www.bing.com/?brdr=1", url);
+	}
+	@Test
+	void funcionanEnlaceBaidu() {
+		driver1.get("http://localhost:3000/buscadores/");
+		WebElement enlaceBaidu = driver1.findElement(By.id("baidu"));
+		enlaceBaidu.click();
+
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("https://www.baidu.com/", url);
+	}
+	
+	@Test
+	void funcionanEnlaceVolver1() {
+		driver1.get("http://localhost:3000/buscadores/");
+		WebElement enlaceVolver = driver1.findElement(By.id("volver"));
+		enlaceVolver.click();
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/buscadores/", url);
+	}
+	
+	@Test
+	void existenEnlacesDePagRedes() {
+		driver1.get("http://localhost:3000/redes_sociales/");
+		WebElement enlaceIg = driver1.findElement(By.id("instagram"));
+		WebElement enlaceFacebook = driver1.findElement(By.id("facebook"));
+		WebElement enlaceTikTok = driver1.findElement(By.id("tiktok"));
+		WebElement enlaceVolver = driver1.findElement(By.id("volver"));		
+		
+		String url = driver1.getCurrentUrl();
+		assertEquals("http://localhost:3000/redes_sociales/", url);
+	}
+	
+	@Test
+	void funcionaEnlaceInstagram() {
+	    driver1.get("http://localhost:3000/redes_sociales/");
+	    WebElement enlaceIg = driver1.findElement(By.id("instagram"));
+	    enlaceIg.click();
+	    
+	    String url = driver1.getCurrentUrl();
+	    assertEquals("https://www.instagram.com/", url);
+	}
+
+	@Test
+	void funcionaEnlaceFacebook() {
+	    driver1.get("http://localhost:3000/redes_sociales/");
+	    WebElement enlaceFacebook = driver1.findElement(By.id("facebook"));
+	    enlaceFacebook.click();
+	    
+	    String url = driver1.getCurrentUrl();
+	    assertEquals("https://www.facebook.com/", url);
+	}
+
+	@Test
+	void funcionaEnlaceTikTok() {
+	    driver1.get("http://localhost:3000/redes_sociales/");
+	    WebElement enlaceTikTok = driver1.findElement(By.id("tiktok"));
+	    enlaceTikTok.click();
+	    
+	    String url = driver1.getCurrentUrl();
+	    assertEquals("https://www.tiktok.com/explore", url);
+	}
+
+	@Test
+	void funcionaEnlaceVolver() {
+	    driver1.get("http://localhost:3000/redes_sociales/");
+	    WebElement enlaceVolver = driver1.findElement(By.id("volver"));
+	    enlaceVolver.click();
+	    
+	    String url = driver1.getCurrentUrl();
+	    assertEquals("http://localhost:3000/redes_sociales/", url);
 	}
 
 }
